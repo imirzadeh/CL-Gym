7d55a4e9622fdbd7b8f60c8a8825f839fe041144
Iman Mirzadeh <iman.kinder@gmail.com>
2021-04-30 15:35:51 -0700

    Add MLP with 4 layers

--- src/cl_gym/backbones/mlp.py
+++ src/cl_gym/backbones/mlp.py
@@ -39,7 +39,8 @@ class FCBlock(nn.Module):
 class MLP2Layers(ContinualBackbone):
     def __init__(self, multi_head=False, num_classes_per_head=None,
                  input_dim=784, hidden_dim_1=200, hidden_dim_2=200,
-                 output_dim=10, dropout_prob=0.0, activation='ReLU'):
+                 output_dim=10, dropout_prob=0.0, activation='ReLU',
+                 include_final_layer_act=False):
         # model variables
         self.input_dim = input_dim
         self.hidden_dim_1 = hidden_dim_1
@@ -49,7 +50,7 @@ class MLP2Layers(ContinualBackbone):
         super(MLP2Layers, self).__init__(multi_head, num_classes_per_head)
         self.block_1 = FCBlock(self.input_dim, self.hidden_dim_1, self.dropout_prob, True, activation)
         self.block_2 = FCBlock(self.hidden_dim_1, self.hidden_dim_2, self.dropout_prob, True, activation)
-        self.block_3 = FCBlock(self.hidden_dim_2, self.output_dim, 0.0, False, activation)
+        self.block_3 = FCBlock(self.hidden_dim_2, self.output_dim, 0.0, include_final_layer_act, activation)
     
     @torch.no_grad()
     def record_activations(self, x):
@@ -70,6 +71,62 @@ class MLP2Layers(ContinualBackbone):
         out = self.block_1(x)
         out = self.block_2(out)
         out = self.block_3(out)
+        if self.multi_head:
+            offset1 = int((task_id - 1) * self.num_classes_per_head)
+            offset2 = int(task_id * self.num_classes_per_head)
+            out[:, :offset1].data.fill_(-10e10)
+            out[:, offset2:].data.fill_(-10e10)
+        return out
+
+
+class MLP4Layers(ContinualBackbone):
+    def __init__(self, multi_head=False, num_classes_per_head=None,
+                 input_dim=784, hidden_dim_1=200, hidden_dim_2=200,
+                 hidden_dim_3=200, hidden_dim_4=200, output_dim=10,
+                 dropout_prob=0.0, activation='ReLU', include_final_layer_act=False):
+        # model variables
+        self.input_dim = input_dim
+        self.hidden_dim_1 = hidden_dim_1
+        self.hidden_dim_2 = hidden_dim_2
+        self.hidden_dim_3 = hidden_dim_3
+        self.hidden_dim_4 = hidden_dim_4
+        self.output_dim = output_dim
+        self.dropout_prob = dropout_prob
+        super(MLP4Layers, self).__init__(multi_head, num_classes_per_head)
+        self.block_1 = FCBlock(self.input_dim, self.hidden_dim_1, self.dropout_prob, True, activation)
+        self.block_2 = FCBlock(self.hidden_dim_1, self.hidden_dim_2, self.dropout_prob, True, activation)
+        self.block_3 = FCBlock(self.hidden_dim_2, self.hidden_dim_3, self.dropout_prob, True, activation)
+        self.block_4 = FCBlock(self.hidden_dim_3, self.hidden_dim_4, self.dropout_prob, True, activation)
+        self.block_5 = FCBlock(self.hidden_dim_4, self.output_dim, 0.0, include_final_layer_act, activation)
+    
+    @torch.no_grad()
+    def record_activations(self, x):
+        result = {}
+        x = x.view(x.shape[0], -1)
+        
+        out = self.block_1(x)
+        result['block_1'] = out.detach().clone()
+        
+        out = self.block_2(out)
+        result['block_2'] = out.detach().clone()
+
+        out = self.block_3(out)
+        result['block_3'] = out.detach().clone()
+        
+        out = self.block_4(out)
+        result['block_4'] = out.detach().clone()
+
+        result['total'] = torch.cat((result['block_1'], result['block_2'],
+                                     result['block_3'], result['block_4']), dim=1)
+        return result
+    
+    def forward(self, x, task_id=None):
+        x = x.view(x.shape[0], -1)
+        out = self.block_1(x)
+        out = self.block_2(out)
+        out = self.block_3(out)
+        out = self.block_4(out)
+        out = self.block_5(out)
         if self.multi_head:
             offset1 = int((task_id - 1) * self.num_classes_per_head)
             offset2 = int(task_id * self.num_classes_per_head)
