ae2dbe2bedec0c23ad4f327690db4cc06a90c35f
Iman Mirzadeh <iman.kinder@gmail.com>
2021-04-30 22:11:00 -0700

    Organize test experiment script

--- src/main.py
+++ src/main.py
@@ -9,15 +9,53 @@ from ray import tune
 from ray.tune.schedulers import AsyncHyperBandScheduler
 
 
-def trial(params):
+def trial_toy_regression(params):
     trial_id = str(uuid.uuid4())
     params['trial_id'] = trial_id
     params['output_dir'] = os.path.join("./outputs/{}".format(trial_id))
     Path(params['output_dir']).mkdir(parents=True, exist_ok=True)
 
     logger = cl.utils.loggers.CometLogger(project_name='debug-2', workspace='cl-boundary', trial_name=trial_id)
-    # benchmark = cl.benchmarks.Toy1DRegression(num_tasks=params['num_tasks'],
-    #                                           per_task_memory_examples=params['per_task_memory_examples'])
+    benchmark = cl.benchmarks.Toy1DRegression(num_tasks=params['num_tasks'],
+                                              per_task_memory_examples=params['per_task_memory_examples'])
+    # benchmark = cl.benchmarks.Toy2DClassification(num_tasks=params['num_tasks'],
+    #                                               per_task_memory_examples=params['per_task_memory_examples'],
+    #                                               per_task_joint_examples=params['per_task_joint_examples'])
+    
+    backbone = cl.backbones.MLP2Layers(input_dim=params['input_dim'],
+                                       hidden_dim_1=params['hidden_1_dim'],
+                                       hidden_dim_2=params['hidden_2_dim'],
+                                       output_dim=params['output_dim'],
+                                       dropout_prob=params.get("dropout_prob", 0.0),
+                                       activation=params.get("activation", 'ReLU'),
+                                       include_final_layer_act=params.get('final_layer_act', False))
+    
+    algorithm = cl.algorithms.ContinualAlgorithm(backbone, benchmark, params)
+    # algorithm = cl.algorithms.OGD(backbone, benchmark, params)
+    
+    metric_manager_callback = cl.callbacks.MetricManager(num_tasks=params['num_tasks'],
+                                                         epochs_per_task=params['epochs_per_task'])
+    visualizer_callback = cl.callbacks.ToyRegressionVisualizer()
+    # visualizer_callback = cl.callbacks.ToyClassificationVisualizer()
+    model_checkpoint_callback = cl.callbacks.ModelCheckPoint()
+    experiment_manager_callback = cl.callbacks.ExperimentManager()
+    trainer = cl.trainer.ContinualTrainer(algorithm, params, logger=logger,
+                                          callbacks=[metric_manager_callback,
+                                                     visualizer_callback,
+                                                     model_checkpoint_callback,
+                                                     experiment_manager_callback])
+    
+    trainer.run()
+    # tune.report(average_loss=metric_collector_callback.get_final_metric())
+
+
+def trial_toy_classification(params):
+    trial_id = str(uuid.uuid4())
+    params['trial_id'] = trial_id
+    params['output_dir'] = os.path.join("./outputs/{}".format(trial_id))
+    Path(params['output_dir']).mkdir(parents=True, exist_ok=True)
+    
+    logger = cl.utils.loggers.CometLogger(project_name='debug-2', workspace='cl-boundary', trial_name=trial_id)
     benchmark = cl.benchmarks.Toy2DClassification(num_tasks=params['num_tasks'],
                                                   per_task_memory_examples=params['per_task_memory_examples'],
                                                   per_task_joint_examples=params['per_task_joint_examples'])
@@ -27,7 +65,8 @@ def trial(params):
                                        hidden_dim_2=params['hidden_2_dim'],
                                        output_dim=params['output_dim'],
                                        dropout_prob=params.get("dropout_prob", 0.0),
-                                       activation=params.get("activation", 'ReLU'))
+                                       activation=params.get("activation", 'ReLU'),
+                                       include_final_layer_act=params.get('final_layer_act', False))
     
     algorithm = cl.algorithms.ContinualAlgorithm(backbone, benchmark, params)
     # algorithm = cl.algorithms.OGD(backbone, benchmark, params)
@@ -48,39 +87,47 @@ def trial(params):
     # tune.report(average_loss=metric_collector_callback.get_final_metric())
 
 
+def trial_rot_mnist(params):
+    trial_id = str(uuid.uuid4())
+    params['trial_id'] = trial_id
+    params['output_dir'] = os.path.join("./outputs/{}".format(trial_id))
+    Path(params['output_dir']).mkdir(parents=True, exist_ok=True)
+    
+    logger = cl.utils.loggers.CometLogger(project_name='debug-2', workspace='cl-boundary', trial_name=trial_id)
+    benchmark = cl.benchmarks.RotatedMNIST(num_tasks=params['num_tasks'],
+                                           per_task_memory_examples=params['per_task_memory_examples'],
+                                           per_task_joint_examples=params['per_task_joint_examples'])
+    
+    backbone = cl.backbones.MLP2Layers(input_dim=params['input_dim'],
+                                       hidden_dim_1=params['hidden_1_dim'],
+                                       hidden_dim_2=params['hidden_2_dim'],
+                                       output_dim=params['output_dim'],
+                                       dropout_prob=params.get("dropout_prob", 0.0),
+                                       activation=params.get("activation", 'ReLU'),
+                                       include_final_layer_act=False)
+    
+    algorithm = cl.algorithms.ContinualAlgorithm(backbone, benchmark, params)
+    # algorithm = cl.algorithms.OGD(backbone, benchmark, params)
+    
+    metric_manager_callback = cl.callbacks.MetricManager(num_tasks=params['num_tasks'],
+                                                         epochs_per_task=params['epochs_per_task'])
+    model_checkpoint_callback = cl.callbacks.ModelCheckPoint()
+    experiment_manager_callback = cl.callbacks.ExperimentManager()
+    trainer = cl.trainer.ContinualTrainer(algorithm, params, logger=logger,
+                                          callbacks=[metric_manager_callback,
+                                                     model_checkpoint_callback,
+                                                     experiment_manager_callback])
+    
+    trainer.run()
+    # tune.report(average_loss=metric_collector_callback.get_final_metric())
+
+
 if __name__ == "__main__":
-    params = {
-        # benchmark
-        'num_tasks': 4,
-        'batch_size_train': 32,#tune.grid_search([8, 16]),
-        'batch_size_memory': 32,
-        'batch_size_validation': 128,
-        'per_task_memory_examples': 10,
-        'per_task_joint_examples': 70,
-    
-        # backbone
-        'input_dim': 2,
-        'hidden_1_dim': 50,
-        'hidden_2_dim': 50,
-        'output_dim': 2,
-        'dropout_prob': 0.00,
-        'activation': 'relu',
-    
-        # algorithm
-        'optimizer': 'SGD', #tune.choice(['SGD', 'Adam']),
-        'momentum': 0.8,
-        'epochs_per_task': 20,
-        'learning_rate': 0.05,#tu:e.loguniform(0.001, 0.05),
-        'learning_rate_decay': 0.94,#tune.uniform(0.7, 0.99),
-        'learning_rate_lower_bound': 0.0005,
-        'criterion': torch.nn.CrossEntropyLoss(),
-        'device': torch.device('cuda' if torch.cuda.is_available() else 'cpu'),
-        'mcsgd_init_pos': 0.9,
-        'mscgd_line_samples': 10,
-    }
-    
+    from params import toy_clf_params, toy_reg_params, rot_mnist_params
     sched = AsyncHyperBandScheduler()
     # analysis = tune.run(trial, metric='average_loss', mode='min',
     #                     scheduler=sched, num_samples=10, config=params)
     # print("Best config is:", analysis.best_config)
-    trial(params)
+    trial_toy_regression(toy_reg_params)
+    # trial_toy_classification(toy_clf_params)
+    # trial_rot_mnist(rot_mnist_params)
---
+++ src/params.py
@@ -0,0 +1,102 @@
+import torch
+
+toy_reg_params = {
+    # benchmark
+    'num_tasks': 3,
+    'batch_size_train': 32,  # tune.grid_search([8, 16]),
+    'batch_size_memory': 32,
+    'batch_size_validation': 128,
+    'per_task_memory_examples': 10,
+    'per_task_joint_examples': 70,
+    
+    # backbone
+    'input_dim': 1,
+    'hidden_1_dim': 10,
+    'hidden_2_dim': 10,
+    'hidden_3_dim': 10,
+    'hidden_4_dim': 10,
+    'output_dim': 1,
+    'dropout_prob': 0.00,
+    'activation': 'tanh',
+    'final_layer_act': True,
+    
+    # algorithm
+    'optimizer': 'SGD',  # tune.choice(['SGD', 'Adam']),
+    'momentum': 0.8,
+    'epochs_per_task': 900,
+    'learning_rate': 0.05,  # tu:e.loguniform(0.001, 0.05),
+    'learning_rate_decay': 0.94,  # tune.uniform(0.7, 0.99),
+    'learning_rate_lower_bound': 0.0005,
+    'criterion': torch.nn.MSELoss(),  # torch.nn.CrossEntropyLoss(),
+    'device': torch.device('cuda' if torch.cuda.is_available() else 'cpu'),
+    'mcsgd_init_pos': 0.9,
+    'mscgd_line_samples': 10,
+}
+
+
+toy_clf_params = {
+    # benchmark
+    'num_tasks': 4,
+    'batch_size_train': 32,  # tune.grid_search([8, 16]),
+    'batch_size_memory': 32,
+    'batch_size_validation': 128,
+    'per_task_memory_examples': 10,
+    'per_task_joint_examples': 70,
+    
+    # backbone
+    'input_dim': 2,
+    'hidden_1_dim': 10,
+    'hidden_2_dim': 10,
+    'hidden_3_dim': 10,
+    'hidden_4_dim': 10,
+    'output_dim': 2,
+    'dropout_prob': 0.00,
+    'activation': 'ReLU',
+    'final_layer_act': False,
+    
+    # algorithm
+    'optimizer': 'SGD',  # tune.choice(['SGD', 'Adam']),
+    'momentum': 0.8,
+    'epochs_per_task': 10,
+    'learning_rate': 0.05,  # tu:e.loguniform(0.001, 0.05),
+    'learning_rate_decay': 0.94,  # tune.uniform(0.7, 0.99),
+    'learning_rate_lower_bound': 0.0005,
+    'criterion':  torch.nn.CrossEntropyLoss(),
+    'device': torch.device('cuda' if torch.cuda.is_available() else 'cpu'),
+    'mcsgd_init_pos': 0.9,
+    'mscgd_line_samples': 10,
+}
+
+
+rot_mnist_params = {
+    # benchmark
+    'num_tasks': 5,
+    'batch_size_train': 32,  # tune.grid_search([8, 16]),
+    'batch_size_memory': 32,
+    'batch_size_validation': 128,
+    'per_task_memory_examples': 10,
+    'per_task_joint_examples': 70,
+    
+    # backbone
+    'input_dim': 784,
+    'hidden_1_dim': 100,
+    'hidden_2_dim': 100,
+    'hidden_3_dim': 100,
+    'hidden_4_dim': 100,
+    'output_dim': 10,
+    'dropout_prob': 0.00,
+    'activation': 'ReLU',
+    'final_layer_act': False,
+    
+    # algorithm
+    'optimizer': 'SGD',  # tune.choice(['SGD', 'Adam']),
+    'momentum': 0.8,
+    'epochs_per_task': 5,
+    'learning_rate': 0.1,  # tu:e.loguniform(0.001, 0.05),
+    'learning_rate_decay': 0.94,  # tune.uniform(0.7, 0.99),
+    'learning_rate_lower_bound': 0.0005,
+    'criterion': torch.nn.CrossEntropyLoss(),
+    'device': torch.device('cuda' if torch.cuda.is_available() else 'cpu'),
+    'mcsgd_init_pos': 0.9,
+    'mscgd_line_samples': 10,
+}
