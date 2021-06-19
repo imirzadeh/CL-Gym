a7e5cfe49ece43a3f7c22c9f2d1119f4d334c99e
Iman Mirzadeh <iman.kinder@gmail.com>
2021-06-19 14:47:33 -0700

    Add CIFAR benchmarks

--- src/cl_gym/benchmarks/__init__.py
+++ src/cl_gym/benchmarks/__init__.py
@@ -4,7 +4,7 @@ from cl_gym.benchmarks.toy_2D_clf import Toy2DClassification
 from cl_gym.benchmarks.toy_1D_reg import Toy1DRegression
 from cl_gym.benchmarks.pamap2 import PAMAP2
 from cl_gym.benchmarks.mixed import MNISTFashionMNIST
-
+from cl_gym.benchmarks.cifar import SplitCIFAR10, SplitCIFAR100
 
 
 __all__ = ['Benchmark',
@@ -14,5 +14,7 @@ __all__ = ['Benchmark',
            'Toy1DRegression',
            'Toy2DClassification',
            'PAMAP2',
-           'MNISTFashionMNIST'
+           'MNISTFashionMNIST',
+           'SplitCIFAR10',
+           'SplitCIFAR100'
            ]
